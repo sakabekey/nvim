532a4ba1871479040a6726d36313bcdb0d6b6da7
Kenki Yonesaka <sakabekey@gmail.com>
2026-06-21 17:17:31 +0900

    refactor: Improve slow Neovim startup - Step 2

--- init.lua
+++ init.lua
@@ -205,7 +205,6 @@ vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
       diagnostics = { globals = { "vim" }, },
-      workspace = { library = vim.api.nvim_get_runtime_file("", true), },
     },
   },
 })
