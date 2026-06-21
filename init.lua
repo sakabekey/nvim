556403d359fa60484233a1e22b48aa3f0fb19c91
Kenki Yonesaka <sakabekey@gmail.com>
2026-06-21 17:11:09 +0900

    refactor: Improve slow Neovim startup - Step 1

--- init.lua
+++ init.lua
@@ -22,6 +22,12 @@ vim.o.updatetime = 250
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Languages for LSP
+local servers = {
+  "lua_ls",
+  "rust_analyzer",
+}
+
 -- Plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
@@ -37,83 +43,160 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-tree/nvim-web-devicons" },
-  { "nvim-mini/mini.statusline" },
-  { "neovim/nvim-lspconfig" },
-  { "folke/which-key.nvim" },
-  { "Saghen/blink.cmp", version = "v1.10.2" },
-  { "stevearc/oil.nvim" },
-  { "nvim-lua/plenary.nvim" },
-  { "nvim-telescope/telescope.nvim" },
-  { "glidenote/memolist.vim" },
-  { "mason-org/mason.nvim" },
-  { "mason-org/mason-lspconfig.nvim" },
-  { "j-hui/fidget.nvim" },
-  { "nvim-tree/nvim-tree.lua" },
-  { "nvim-mini/mini.surround" },
-  { "nvim-mini/mini.align" },
-  { "lewis6991/gitsigns.nvim" },
-  { "nvim-mini/mini.pairs" },
-  { "nvim-mini/mini.cursorword" },
-}, {
-  defaults = {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     lazy = false,
-  },
-})
-
--- Theme
-vim.cmd.colorscheme("catppuccin-mocha")
-
--- Languages for LSP
-local servers = {
-  "lua_ls",
-  "rust_analyzer",
-}
-
--- Setup
-require("oil").setup()
-require("which-key").setup()
-require("mini.statusline").setup()
-require("fidget").setup()
-require("nvim-tree").setup()
-require("mini.surround").setup()
-require("mini.align").setup()
-require("mini.pairs").setup()
-require("mini.cursorword").setup()
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = servers,
-  handlers = {
-    function(server)
-      require("lspconfig")[server].setup({})
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
-})
-require("telescope").setup({
+  { "nvim-tree/nvim-web-devicons" },
+  {
+    "nvim-mini/mini.statusline",
+    event = "VeryLazy",
+    config = function()
+      require("mini.statusline").setup()
+    end,
+  },
+  { "neovim/nvim-lspconfig" },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("which-key").setup()
+    end,
+  },
+  {
+    "Saghen/blink.cmp",
+    version = "v1.10.2",
+    event = "InsertEnter",
+    config = function()
+      require("blink.cmp").setup({
+        keymap = {
+          preset = "default",
+        },
+        completion = {
+          documentation = { auto_show = true },
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    cmd = "Oil",
+    config = function()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
+  },
+  { "nvim-lua/plenary.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          preview = { treesitter = false },
+        },
+        pickers = {
+          find_files = {
+            path_display = { "relative" },
+          },
+          buffers = {
+            path_display = { "smart" },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "glidenote/memolist.vim",
+    init = function()
+      vim.g.memolist_memo_suffix = "md"
+      vim.g.memolist_filename_date = "%Y%m%d_"
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    event = "BufReadPre",
+    dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = servers,
+        handlers = {
+          function(server)
+            require("lspconfig")[server].setup({})
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    event = "LspAttach",
+    config = function()
+      require("fidget").setup()
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeOpen", "NvimTreeToggle", "NvimTreeFindFile" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+    end,
+  },
+  {
+    "nvim-mini/mini.surround",
+    event = "VeryLazy",
+    config = function()
+      require("mini.surround").setup()
+    end,
+  },
+  {
+    "nvim-mini/mini.align",
+    event = "VeryLazy",
+    config = function()
+      require("mini.align").setup()
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "nvim-mini/mini.pairs",
+    event = "InsertEnter",
+    config = function()
+      require("mini.pairs").setup()
+    end,
+  },
+  {
+    "nvim-mini/mini.cursorword",
+    event = "VeryLazy",
+    config = function()
+      require("mini.cursorword").setup()
+    end,
+  },
+}, {
   defaults = {
-    preview = { treesitter = false, },
-  },
-  pickers = {
-    find_files = {
-      path_display = { "relative" },
-    },
-    buffers = {
-      path_display = { "smart" },
-    },
-  },
-})
-require("blink.cmp").setup({
-  keymap = {
-    preset = "default",
-  },
-  completion = {
-    documentation = { auto_show = true },
-  },
-})
-require("oil").setup({
-  view_options = {
-    show_hidden = true,
+    lazy = true,
   },
 })
 
@@ -152,19 +235,22 @@ vim.g.memolist_memo_suffix = "md"
 vim.g.memolist_filename_date = "%Y%m%d_"
 
 -- Telescope keymap
-local telescope_builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Telescope help tags" })
-vim.keymap.set("n", "<leader>fk", telescope_builtin.keymaps, { desc = "Telescope key maps" })
-vim.keymap.set("n", "<leader>fs", telescope_builtin.builtin, { desc = "Telescope builtin" })
-vim.keymap.set({ "n", "v" }, "<leader>fw", telescope_builtin.grep_string, { desc = "Telescope grep string" })
-vim.keymap.set("n", "<leader>fd", telescope_builtin.diagnostics, { desc = "Telescope diagnostics" })
-vim.keymap.set("n", "<leader>fr", telescope_builtin.resume, { desc = "Telescope resume" })
-vim.keymap.set("n", "<leader>f.", telescope_builtin.oldfiles, { desc = "Telescope old files" })
-vim.keymap.set("n", "<leader>fc", telescope_builtin.commands, { desc = "Telescope commands" })
-vim.keymap.set('n', '<leader><leader>', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+local function telescope_builtin()
+  return require("telescope.builtin")
+end
+
+vim.keymap.set("n", "<leader>ff", function() telescope_builtin().find_files() end, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", function() telescope_builtin().live_grep() end, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", function() telescope_builtin().buffers() end, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", function() telescope_builtin().help_tags() end, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>fk", function() telescope_builtin().keymaps() end, { desc = "Telescope key maps" })
+vim.keymap.set("n", "<leader>fs", function() telescope_builtin().builtin() end, { desc = "Telescope builtin" })
+vim.keymap.set({ "n", "v" }, "<leader>fw", function() telescope_builtin().grep_string() end, { desc = "Telescope grep string" })
+vim.keymap.set("n", "<leader>fd", function() telescope_builtin().diagnostics() end, { desc = "Telescope diagnostics" })
+vim.keymap.set("n", "<leader>fr", function() telescope_builtin().resume() end, { desc = "Telescope resume" })
+vim.keymap.set("n", "<leader>f.", function() telescope_builtin().oldfiles() end, { desc = "Telescope old files" })
+vim.keymap.set("n", "<leader>fc", function() telescope_builtin().commands() end, { desc = "Telescope commands" })
+vim.keymap.set("n", "<leader><leader>", function() telescope_builtin().buffers() end, { desc = "Telescope buffers" })
 
 -- LSP keymap
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -179,14 +265,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     map("gd", vim.lsp.buf.definition, "definition")
     map("gD", vim.lsp.buf.declaration, "declaration")
     map("gr", function()
-      telescope_builtin.lsp_references({
+      telescope_builtin().lsp_references({
         path_display = function(_, path)
           return vim.fn.fnamemodify(path, ":.")
         end,
       })
     end, "references")
-    map("gi", telescope_builtin.lsp_implementations, "implementation")
-    map("gy", telescope_builtin.lsp_type_definitions, "type definition")
+    map("gi", function() telescope_builtin().lsp_implementations() end, "implementation")
+    map("gy", function() telescope_builtin().lsp_type_definitions() end, "type definition")
     -- information
     map("K", vim.lsp.buf.hover, "hover")
     map("zK", vim.lsp.buf.signature_help, "signature")
@@ -197,7 +283,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     map("[d", function() vim.diagnostic.jump({ count = -1 }) end, "prev diagnostic")
     map("]d", function() vim.diagnostic.jump({ count = 1 }) end, "next diagnostic")
     map("gl", vim.diagnostic.open_float, "line diagnostic")
-    map("gL", telescope_builtin.diagnostics, "diagnostics list")
+    map("gL", function() telescope_builtin().diagnostics() end, "diagnostics list")
     -- Rust
     map("gz", vim.lsp.buf.code_action, "rust action")
   end,
